449d596232ad2527c673c3a70ea1f554dc3ae6ef
DaniloGarrido1 <129185768+DaniloGarrido1@users.noreply.github.com>
2023-07-03 20:11:03 -0400

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -1,5 +1,5 @@
 import pywhatkit
-
+# pywhatkit es una biblioteca de python que permite diferentes funcionalidades, entre ellas el enviar mensajes por whatsapp
 
 def enviar_mensaje_despues(numero, mensaje, hora, minuto):
     # Enviar el mensaje
