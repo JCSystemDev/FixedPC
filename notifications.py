d505826ac4907fae151069d452257763babba4a3
Juan Carlos Gallardo <99374220+JCSystemDev@users.noreply.github.com>
2023-07-06 10:01:34 -0400

    Delete notifications.py

--- notifications.py
+++
@@ -1,6 +0,0 @@
-import pywhatkit
-# pywhatkit es una biblioteca de python que permite diferentes funcionalidades, entre ellas el enviar mensajes por whatsapp
-
-def enviar_mensaje_despues(numero, mensaje, hora, minuto):
-    # Enviar el mensaje
-    pywhatkit.sendwhatmsg(numero, mensaje, hora, minuto)
